72f890bd0af98773d81f353b3f97d0914e524058
Alexandre Silva <alexandrerays@gmail.com>
2023-02-25 11:15:08 -0300

    Run algo for multiple images

--- inference.py
+++ inference.py
@@ -10,27 +10,23 @@ from lib.tools import create_dir
 from lib.dataset import DatasetInitializer
 from lib.models import ModelsBuilder
 import argparse
-from PIL import Image
 import numpy as np
 import io
-
-step = 0.05
 
 BATCH_SIZE = 1
 IMG_WIDTH = 1024
 IMG_HEIGHT = 1024
 OUTPUT_CHANNELS = 3
+INTENSITY = 0.9
 
-weights_path = "./weights/"
+# Config paths
+weights_path = './weights/'
+input_path = './runs/input/'
+output_path = './runs/output/'
 
 use_transmission_map = False #@param{type: "boolean"}
 use_gauss_filter = False #@param{type: "boolean"}
 use_resize_conv = False #@param{type: "boolean"}
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--case", required=True, help="path to input image")
-args = vars(ap.parse_args())
-test_case = args["case"]
 
 datasetInit = DatasetInitializer(image_height=IMG_HEIGHT, image_width=IMG_WIDTH)
 datasetInit.dataset_path = './dataset/'
@@ -53,27 +49,26 @@ trainer.configure_checkpoint(weights_path = weights_path, load_optimizers=False)
 
 trainer.load_config()
 
-intensity_path = f'./runs/output/{test_case}/'
-create_dir(intensity_path)
-file_path = f'./runs/input/{test_case}.png'
+# loop through all files in the folder
+for filename in os.listdir(input_path):
+    # check if the file is a PNG file
+    if filename.endswith('.png'):
+        filename_with_path = os.path.join(input_path, filename)
 
-image_clear = tf.io.decode_png(tf.io.read_file(file_path), channels=3)
+        image_clear = tf.io.decode_png(tf.io.read_file(filename_with_path), channels=3)
 
-original_dimensions = (image_clear.shape[1], image_clear.shape[0])
-print(original_dimensions)
+        # Print original dimensions
+        original_dimensions = (image_clear.shape[1], image_clear.shape[0])
+        print(original_dimensions)
 
-image_clear, _ = datasetInit.preprocess_image_test(image_clear, 0)
+        # Apply fog
+        image_clear, _ = datasetInit.preprocess_image_test(image_clear, 0)
+        fig = plot_clear2fog_intensity_v2(generator_clear2fog, image_clear, INTENSITY, normalized_input=True)
 
+        # Save Images
+        fig.set_size_inches(10, 10)
+        fig_name_resized = os.path.join(output_path, "intensity_{:0.2f}_{}".format(INTENSITY, filename))
+        fig.savefig(fig_name_resized, bbox_inches='tight', pad_inches=0, dpi=200)
+        plt.close(fig)
 
-fig = plot_clear2fog_intensity_v2(generator_clear2fog, image_clear, 0.9, normalized_input=True)
-fig.set_size_inches(10, 10)
-#ind = ind+20
-fig_name_resized = os.path.join(intensity_path, "intensity_{:0.2f}_resized.jpg".format(0.9))
-fig_name = os.path.join(intensity_path, "intensity_{:0.2f}.jpg".format(0.9))
-fig.savefig(fig_name_resized, bbox_inches='tight', pad_inches=0, dpi=200)
-plt.close(fig)
-
-image = Image.open(fig_name_resized)
-new_image = image.resize(original_dimensions)
-new_image.save(fig_name)
 
